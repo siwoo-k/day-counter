083c7bc6602c76e2359431ffc44ec2d3725e3f20
Siwoo Kim <siwookim435@gmail.com>
2024-11-09 03:20:52 +0900

    Finished product and embedded working video and audio play

---
+++ script.js
@@ -0,0 +1,46 @@
+async function checkPass() {
+  let inputPswd = document.getElementById("validate")
+                          .value;
+  let correctPswd = "1525";
+  if (inputPswd === correctPswd) {
+    showLetter();
+  } else {
+    alert("Incorrect password!");
+  }
+}
+
+async function showLetter() {
+  document.getElementById("container")
+    .style.display = "flex";
+  document.getElementById("prompt")
+    .style.display = "none";
+  document.querySelectorAll('.stoo').forEach((stoo) => {
+    stoo.classList.add('toggle');
+  });
+}
+
+async function showVideo() {
+  document.getElementById("wait-audio").volume = 0.03;
+  let video = document.getElementById("hamster-video");
+  video.style.display = "flex";
+  video.play();
+  video.scrollIntoView();
+}
+
+function main() {
+  var audio = document.getElementById("wait-audio");
+  if (audio.paused) {
+    audio.play();
+  }
+  audio.volume = 0.4;
+
+  let input = document.getElementById("validate");
+  input.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+      checkPass();
+      event.preventDefault();
+    }
+  });
+}
+
+window.onload = main();
